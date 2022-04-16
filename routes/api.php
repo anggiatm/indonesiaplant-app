a6ba0fdaf7ae5b0a439e02f5fb60551a1db9d88b
anggiatm <anggiat17@gmail.com>
2022-04-16 15:18:09 +0700

    add api token

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public Route
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+
+// Protected Route
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/products', [ProductController::class, 'store']);
+Route::middleware('auth:sanctum')->put('/products/{id}', [ProductController::class, 'update']);
+Route::middleware('auth:sanctum')->delete('/products/{id}', [ProductController::class, 'destroy']);
