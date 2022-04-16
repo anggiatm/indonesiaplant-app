083317a026610d2a7468b706bc070971a18f4453
anggiatm <anggiat17@gmail.com>
2022-04-17 04:45:27 +0700

    add model and migration

---
+++ database/migrations/2022_04_16_213419_create_promos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('promos', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->decimal('discount', 3, 2);
+            $table->dateTime('valid_until');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('promos');
+    }
+}
