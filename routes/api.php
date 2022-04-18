e78977f85875b15958a76c91ca50498296696489
anggiatm <anggiat17@gmail.com>
2022-04-18 22:04:40 +0700

    add cart route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PromoController;
+use App\Models\Cart;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,19 +20,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Public Route
-//USERS
+// USERS PUBLIC
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-//PRODUCTS
+// PRODUCTS PUBLIC
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('products/search/{name}', [ProductController::class, 'search']);
-
-//ORDERS
-
-// Protected Route
+// PRODUCTS PROTECTED
 Route::middleware('auth:sanctum')->post('/products', [ProductController::class, 'store']);
 Route::middleware('auth:sanctum')->put('/products/{id}', [ProductController::class, 'update']);
 Route::middleware('auth:sanctum')->delete('/products/{id}', [ProductController::class, 'destroy']);
+
+// CART PUBLIC
+// CART PROTECTED
+Route::middleware('auth:sanctum')->get('my-cart/{id}', [CartController::class, 'index']);
+Route::middleware('auth:sanctum')->put('my-cart-update/{id}', [CartController::class, 'update']);
+Route::middleware('auth:sanctum')->post('my-cart-store/{id}', [CartController::class, 'store']);
+Route::middleware('auth-sanctum')->delete('my-cart', [CartController::class, 'destroy']);
